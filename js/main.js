781a75f657495dfda36f9c14b07038bdf03eee5f
Saroj Shrestha <practise.saroj@gmail.com>
2024-02-06 21:38:51 -0500

    UI change

--- js/main.js
+++ js/main.js
@@ -69,52 +69,11 @@
     });
 
 
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+    
 
 
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        // $('.btn-play').click(function () {
-        //     $videoSrc = $(this).data("src");
-        // });
-        // console.log($videoSrc);
 
-        // $('#videoModal').on('shown.bs.modal', function (e) {
-        //     $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        // })
-
-        // $('#videoModal').on('hide.bs.modal', function (e) {
-        //     $("#video").attr('src', $videoSrc);
-        // })
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
+    
     
 })(jQuery);
 
